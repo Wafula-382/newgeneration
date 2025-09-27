9e6021259a78f748833f948bffa836c453269fae
Wafula-382 <gabwafula382@gmail.com>
2025-09-27 07:44:23 +0300

    Add files via upload

---
+++ wafula (2).py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+import math
+print(math.sqrt(16))
+wafula = "I am Learning Python"
+print(wafula, "With math applications, python is fun")")
+print("I am enjoying Python programming")
+
+print("Python is fun")
